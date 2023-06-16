2b832b752f0240d8da6bcc9e963bd21f5a385dc9
Ajax Lima <ajaxlima94@gmail.com>
2023-06-16 17:26:24 -0300

    Feat: init project

---
+++ src/main/java/fadesp/desafio/tec/desafio/DesafioTecnicoFadespApplication.java
@@ -0,0 +1,13 @@
+package fadesp.desafio.tec.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoFadespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoFadespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fadesp/desafio/tec/desafio/DesafioTecnicoFadespApplicationTests.java
@@ -0,0 +1,13 @@
+package fadesp.desafio.tec.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoFadespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
