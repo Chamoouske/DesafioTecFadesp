b454dc45d595c9ff8c20d611b9cb545b3da68b6a
Ajax Lima <ajaxlima94@gmail.com>
2025-03-01 14:18:40 -0300

    refactor: corrigindo a PaymentStatusFactory para respeitar o OCP do SOLID

--- src/main/java/fadesp/desafio/tec/desafio/payment/designpattern/factory/PaymentStatusFactory.java
+++ src/main/java/fadesp/desafio/tec/desafio/payment/designpattern/factory/PaymentStatusFactory.java
@@ -2,23 +2,31 @@ package fadesp.desafio.tec.desafio.payment.designpattern.factory;
 
 import fadesp.desafio.tec.desafio.payment.designpattern.state.*;
 
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 public class PaymentStatusFactory {
+    private static PaymentStatusFactory instance;
+    private final HashMap<String, Callable<PaymentStatus>> statusMap = new HashMap<>();
+
     private PaymentStatusFactory() {
     }
 
-    public static PaymentStatus create(String status) {
-        PaymentStatus state = new PendenteDeProcessamentoState();
-        if (Objects.nonNull(status)) {
-            state = switch (status) {
-                case "falha" -> new ProcessamentoComFalhaState();
-                case "sucesso" -> new ProcessamentoComSucessoState();
-                case "delete" -> new DeletePaymentState();
-                default -> new PendenteDeProcessamentoState();
-            };
-        }
+    public static PaymentStatus create(String status) throws Exception {
+        Callable<PaymentStatus> state = getInstance().statusMap.getOrDefault(status, PendenteDeProcessamentoState::new);
 
-        return state;
+        return state.call();
+    }
+
+    public void register(String status, Callable<PaymentStatus> state) {
+        statusMap.put(status, state);
+    }
+
+    public static PaymentStatusFactory getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new PaymentStatusFactory();
+        }
+        return instance;
     }
 }
